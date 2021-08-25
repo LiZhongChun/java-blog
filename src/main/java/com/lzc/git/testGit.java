cd3af131e66f4534e3542d3db9d6371c7f38f652
Lzc <1711004660@qq.com>
2021-08-25 21:34:47 +0800

    second commit

---
+++ src/main/java/com/lzc/git/testGit.java
@@ -0,0 +1,12 @@
+package com.lzc.git;
+
+/**
+ * @author shkstart
+ * @create 2021-08-25-21:33
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
